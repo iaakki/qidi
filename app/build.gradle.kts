9f4042aa3bebe2b3e64f3b3fd528025a0c6b4368
iaakki <iaakki@outlook.com>
2026-07-17 19:40:40 +0300

    Add Shizuku shell service runner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,20 @@ android {
         versionCode = 1
         versionName = "0.1.0"
     }
+
+    buildFeatures {
+        aidl = true
+        buildConfig = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
---
+++ app/src/main/aidl/app/qidi/IQidiShellService.aidl
@@ -0,0 +1,6 @@
+package app.qidi;
+
+interface IQidiShellService {
+    String run(String command);
+    void destroy();
+}
